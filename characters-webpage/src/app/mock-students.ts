afca63a3b1772df29b5d58923019ce8c07fb079d
TJGit11 <74613952+TJGit11@users.noreply.github.com>
2020-12-11 20:42:15 -0600

    mensaje

---
+++ characters-webpage/src/app/mock-students.ts
@@ -0,0 +1,14 @@
+import { Student } from './student';
+
+export const STUDENTS: Student[] = [
+  { id: 1, name: 'Cloud', major: 'Warrior'},
+  { id: 2, name: 'Aerith', major: 'Healer'},
+  { id: 3, name: 'Killua', major: 'Assassin'},
+  { id: 4, name: 'Wakka', major: 'Blitzballer'},
+  { id: 5, name: 'Lulu', major: 'Black Mage'},
+  { id: 6, name: 'Auron', major: 'Warrior'},
+  { id: 7, name: 'Barrett', major: 'Gunner'},
+  { id: 8, name: 'Amarant', major: 'Assassin'},
+  { id: 9, name: 'Yusuke', major: 'Spirit Detective'},
+  { id: 10, name: 'Freya', major: 'Dragoon'}
+];
